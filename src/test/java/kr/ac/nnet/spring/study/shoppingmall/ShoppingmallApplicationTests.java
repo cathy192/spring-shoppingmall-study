3833b7ec3ad99cb2ce93211db442624cefae18ae
dailyworker <workingssu@gmail.com>
2021-01-07 18:51:32 +0900

    초기 프로젝트 세팅

---
+++ src/test/java/kr/ac/nnet/spring/study/shoppingmall/ShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.nnet.spring.study.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
