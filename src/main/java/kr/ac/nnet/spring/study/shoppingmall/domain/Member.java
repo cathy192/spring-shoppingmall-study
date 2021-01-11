9c349a0295a6d958d4007d5b1f5119cec8a9920d
cathy192 <korea@2047>
2021-01-11 10:17:01 +0900

    domain에서 Member 클래스와 속성 추가

---
+++ src/main/java/kr/ac/nnet/spring/study/shoppingmall/domain/Member.java
@@ -0,0 +1,21 @@
+package kr.ac.nnet.spring.study.shoppingmall.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String address; // 차후 수정
+
+
+}
